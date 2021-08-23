9fc8a6a561045ec0e62cb0232f61f78a522ef7d9
Muhammed <muhammadburhan964@gmail.com>
2021-08-23 15:53:37 +0300

    commit tree e0ba9f2c935076a2beb684c8687cdd3a217c03fe

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 // Note: createStore and candyReducer must be exported for the tests to run
 
-export function createStore() {
+export function createStore(reducer) {
   // write your createStore code here
+  let state;
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+  function getState() {
+    return state;
+  }
+  return { dispatch, getState };
 }
 
 export function candyReducer(state = [], action) {
@@ -24,3 +33,16 @@ function render() {
 
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
+const store = createStore(candyReducer);
+store.dispatch({ type: "@@IN" });
+
+// note u must reomve the exports inorder for the form to work
+const form = document.getElementById("form");
+function logSubmit(event) {
+  store.dispatch({
+    type: "candies/add",
+    candy: document.getElementById("candy").value,
+  });
+  event.preventDefault();
+}
+form.addEventListener("submit", logSubmit);
